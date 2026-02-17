2e3f01206497eb1c64b90114e613204e77457251
prathamawasthi84 <prat7986@gmail.com>
2026-02-17 21:45:23 +0530

    initial spring commit

---
+++ src/test/java/com/example/Blog/API/BlogApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Blog.API;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
